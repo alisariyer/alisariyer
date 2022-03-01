e69f9596110e514255954e867f90cf0fa6dbdc4d
Ali Sariyer <alisariyer@gmail.com>
2022-03-01 21:32:19 +0000

    1. Add card elements 2. Style card title, img, buttons etc.

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -4,23 +4,26 @@ import data from "../data.json";
 
 export default function Projects() {
   const projects = data.projects;
+
   const projectCards = projects.map(project => {
     return (
       <article className="card" key={project.id}>
+        <Dots backgroundColor="#ffffff36"/>
         <div className="card-img--container">
           <Dots backgroundColor="#356EFF" isTriangle={true}/>
-          <img className="card-img" src={project.imageUrl} alt={project.name} />
+          <img className="card-img" src={`./images/${project.image}`} alt={project.name} />
         </div>
-        <div className="card-title--container">
+        <div className="card-content--container">
+          <div className="card-title--container">
           <Dots backgroundColor="#356EFF" />
-          <h4 className="card-title">{project.name}</h4>
-          <p className="card-detail">{project.detail}</p>
+          <h4 className="card-title regular">{project.name}</h4>
+          </div>
+          <p className="card-detail extra-light">{project.detail}</p>
         </div>
-        <div className="card-btn-container">
-          <button className="btn btn-github" type="button">Github Repo</button>
-          <button className="btn btn-live" type="button">Live Version</button>
+        <div className="card-btn--container">
+          <button className="btn btn-github regular" type="button">Github Repo</button>
+          <button className="btn btn-live regular" type="button">Live Version</button>
         </div>
-        <Dots backgroundColor="#ffffff36" />
       </article>
     )
   })
