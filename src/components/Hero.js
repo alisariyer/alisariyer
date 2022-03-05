c3787980492c2c3c4d1951f0fca70e7b48d5c25a
Ali Sariyer <alisariyer@gmail.com>
2022-03-05 11:18:02 +0000

    Update hero texts

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -8,12 +8,13 @@ export default function Hero() {
         <div className="hero-bg-dots">
           <Dots backgroundColor="#ffffff36" rotate={45} />
         </div>
-        <h2 className="hero-title">WELCOME TO MY GITHUB PAGE</h2>
+        <h2 className="hero-title">WELCOME TO MY PORTFOLIO</h2>
         <div className="hero-content">
           <Dots backgroundColor="#356EFF" rotate={0} />
           <p className="extra-light">
             I'm a <span className="blue regular">front-end web developer</span>,
-            interested in CSS, JavaScript, and React.js
+            making applications with HTML5, CSS3, Bootstrap, JavaScript and React.js.
+            Collaborative to work on crazy projects. Some of my projects are below...
           </p>
         </div>
       </div>
