bf41cc89ad18e2c69ebff2b3dfc1d6bd2e759fb4
Ali Sariyer <alisariyer@gmail.com>
2022-03-24 14:28:15 +0000

    Update card image height

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,7 +5,7 @@ export default function Header({handleMenu, isMenuOpen, className}) {
   return (
     <header className="main-header">
       <div className="container">
-        <h1><a href="#top" className="mask-linear brand-link">ALI SARIYER</a></h1>
+        <h1><a href="/" className="mask-linear brand-link">ALI SARIYER</a></h1>
         <Dots className={`${className ? className : ''}`} rotate={0} handleMenu={handleMenu} isMenuOpen={isMenuOpen}/>
       </div>
     </header>
